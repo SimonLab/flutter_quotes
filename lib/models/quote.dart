5f3d3d10d1d8e46830d7bc7c6710d1f14707c8ed
SimonLab <simon.labondance@gmail.com>
2021-03-17 18:08:22 +0000

    parse json quotes to dart quote objects + use futureBuilder to build the listview widget

--- lib/models/quote.dart
+++ lib/models/quote.dart
@@ -1,8 +1,10 @@
 class Quote {
   final String author;
   final String text;
-  final String tags;
-  final String source;
 
-  Quote({this.author, this.text, this.tags, this.source});
+  Quote({this.author, this.text});
+
+  Quote.fromJson(Map<String, dynamic> json)
+      : author = json['author'],
+        text = json['text'];
 }
--- lib/screens/quotes/quotes.dart
+++ lib/screens/quotes/quotes.dart
@@ -1,27 +1,43 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_quotes/models/quote.dart';
+import 'package:http/http.dart' as http;
 
 class Quotes extends StatelessWidget {
-  final List<Quote> quotes = [
-    Quote(author: 'Author1', text: 'first quote'),
-    Quote(author: 'Author2', text: 'second quote')
-  ];
-
   @override
   Widget build(context) {
     return Scaffold(
-      body: listQuotes(quotes),
-    );
+        body: FutureBuilder(
+      future: fetchQuotes(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return listQuotes(snapshot.data);
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
+    ));
   }
- 
+
   Widget listQuotes(List<Quote> quotes) {
     return ListView.builder(
       itemCount: quotes.length,
       itemBuilder: (context, index) {
-        return ListTile(title: Text('${quotes[index].author} - ${quotes[index].text}'));
+        return ListTile(
+            title: Text('${quotes[index].author} - ${quotes[index].text}'));
       },
     );
   }
 
-  
+  Future<List<Quote>> fetchQuotes() async {
+    final response = await http.get(Uri.parse(
+        'https://raw.githubusercontent.com/dwyl/quotes/master/quotes.json'));
+    if (response.statusCode == 200) {
+      // parse to json
+      final parsed = jsonDecode(response.body).cast<Map<String, dynamic>>();
+      return parsed.map<Quote>((json) => Quote.fromJson(json)).toList();
+    } else {
+      return [];
+    }
+  }
 }
